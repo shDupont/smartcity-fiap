7791b49044c4468ab02ef2e91d271e2308aae24c
Letícia Dupont <leticia.dupont20@gmail.com>
2020-09-21 20:50:28 -0300

    make it better

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,5 +125,5 @@ MEDIA_ROOT = '/blog/media/'
 
 MEDIA_URL = '/media/'
 
-import django_heroku
+
 django_heroku.settings(locals())
