b07c8ee2e0fb7f20f96a193c8d8e77e052952de7
Letícia Dupont <leticia.dupont20@gmail.com>
2020-09-21 21:42:05 -0300

    creting .env runtime.txt

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import index, news, evento
 urlpatterns = [
-    path('index', index, name='index'),
+    path('', index, name='index'),
     path('news', news, name="news" ),
     path('eventos', evento, name="eventos")
 ]
