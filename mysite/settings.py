1fcc99569c3a6b5b56685ed29f7140c8031f83b5
Letícia Dupont <leticia.dupont20@gmail.com>
2020-09-21 22:55:54 -0300

    removing my_site folder

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.blogConfig',
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
